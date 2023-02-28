5e7a180324a5b9933f013bc58bb7a984f8942d6f
Nguyen Thai <thai00436@gmail.com>
2023-02-28 13:55:28 +0700

    thai first commit

--- admin/Content/js/jsondemo.js
+++
@@ -1,8 +0,0 @@
-window.dataJson = {
-    users: {
-        id: "1", name: 'Xã Thiệu Trung, Huyện Thiệu Thóa', account: 'thieutrung_thanhhoa', group: 'Thanh Hóa',
-        id: "2",name: 'Xã Hoằng Phụ, Huyện Hoằng Hóa', account: 'hoangphu_hoanghoa', group: 'Thanh Hóa',
-        id: "3", name: 'Xã Hoằng Tiến, Huyện Hoằng Hóa', account: 'hoangtien_hoanghoa', group: 'Thanh Hóa'
-    }
-      
-}
